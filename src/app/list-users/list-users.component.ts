2da4506fe996b48d57ed8acda7eb82232a01eb84
Jahanvi <jahanvimp128@gmail.com>
2023-09-09 18:04:48 +0530

    service

--- src/app/list-users/list-users.component.ts
+++ src/app/list-users/list-users.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-list-users',
@@ -18,18 +19,25 @@ export class ListUsersComponent {
   visible:boolean = false
   data:any = ""
 
-  constructor(private httpClient:HttpClient, private toastr:ToastrService, private router:Router){
+  constructor(private userService:UserService, private toastr:ToastrService, private router:Router){
     this.getAllUsers()
   }
 
   getAllUsers(){
     // let apiResponse:any = {}
-    console.log("List User api called...");
+    // console.log("List User api called...");
 
-    this.httpClient.get("https://demopass.onrender.com/getallusers").subscribe(resp => {
-      this.apiResponse = resp
+    // this.httpClient.get("https://demopass.onrender.com/getallusers").subscribe(resp => {
+    //   this.apiResponse = resp
       // console.log(apiResponse);
       // console.log(apiResponse.data);
+    //   this.users = this.apiResponse.data
+    // },err => {
+
+    // })
+
+    this.userService.getAllUsers().subscribe(resp => {
+      this.apiResponse = resp
       this.users = this.apiResponse.data
     },err => {
 
@@ -38,7 +46,14 @@ export class ListUsersComponent {
 
   deleteUser(userId:string){
     // alert("Delete"+" => "+userId);
-    this.httpClient.delete("https://demopass.onrender.com/deleteuser/"+userId).subscribe(resp => {
+    // this.httpClient.delete("https://demopass.onrender.com/deleteuser/"+userId).subscribe(resp => {
+    //   this.getAllUsers();
+    //   this.toastr.success("","User Removed",{
+    //     timeOut:3000
+    //   })
+    // })
+
+    this.userService.deleteUser(userId).subscribe(resp => {
       this.getAllUsers();
       this.toastr.success("","User Removed",{
         timeOut:3000
@@ -51,9 +66,16 @@ export class ListUsersComponent {
   }
 
   showDialog(userId:string){
-    this.uId = userId
-    this.httpClient.get("https://demopass.onrender.com/getuserbyid/"+userId).subscribe(resp => {
-      // console.log(resp);
+    // this.uId = userId
+    // this.httpClient.get("https://demopass.onrender.com/getuserbyid/"+userId).subscribe(resp => {
+    //   // console.log(resp);
+    //   this.data = resp
+    //   this.firstName = this.data.data.firstName
+    //   this.email = this.data.data.email
+    //   this.visible = true
+    // })
+
+    this.userService.showdialog(userId).subscribe(resp => {
       this.data = resp
       this.firstName = this.data.data.firstName
       this.email = this.data.data.email
---
+++ src/app/user.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private httpClient:HttpClient) {
+
+   }
+
+   addUser(){
+
+   }
+
+   deleteUser(userId:String):Observable<any>{
+    return this.httpClient.delete("https://demopass.onrender.com/deleteuser/"+userId)
+   }
+
+   getUserById(userId:String):Observable<any>{
+    return this.httpClient.get("https://demopass.onrender.com/getuserbyid"+userId)
+   }
+
+   getAllUsers():Observable<any>{
+    return this.httpClient.get("https://demopass.onrender.com/getallusers")
+   }
+
+   showdialog(userId:string):Observable<any>{
+    return this.httpClient.get("https://demopass.onrender.com/getuserbyid/"+userId)
+   }
+  }
